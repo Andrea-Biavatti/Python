fea017e385ded29a97ae8ffe62490e79f06b49e7
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2020-05-31 06:46:27 -0300

    StringCondition

--- StringCondition/StringCondition.py
+++ StringCondition/StringCondition.py
@@ -1,11 +1,11 @@
-print("Heitor Bisneto [Versão 1.0]")
-print("(c) - 2019 Bisneto Inc. Todos os direitos reservados")
+print("Heitor Bisneto [Versão 1.1]")
+print("(c) - 2019-2020 Bisneto Inc. Todos os direitos reservados")
 print("")
 Nome = str(input("Escreva algo usando letras: "))
 
 if Nome == "Moqueca" or Nome == "moqueca":
     print("Categoria: Comida")
-if Nome == "Ford" or Nome == "ford":
+elif Nome == "Ford" or Nome == "ford":
     print("Categoria: Marca de carro")
 else:
     print("Categoria: Não Cadastrada")
