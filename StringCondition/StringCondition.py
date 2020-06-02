6d491f3fe50030c6df8880972aef5566f7df577e
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2020-06-01 23:04:15 -0300

    StringCondition

--- StringCondition/StringCondition.py
+++ StringCondition/StringCondition.py
@@ -1,11 +1,17 @@
-print("Heitor Bisneto [Versão 1.1]")
-print("(c) - 2019-2020 Bisneto Inc. Todos os direitos reservados")
+from datetime import date
+AnoAtual = date.today().year
+SoftwareName = "STRINGCONDITION"
+
+print("Nome:", SoftwareName)
+print("Versão: 1.1")
+print("Criado por: Heitor Bisneto")
+print("Copyright © 2019 -", AnoAtual, "Bisneto. All rights reserved.")
 print("")
 Nome = str(input("Escreva algo usando letras: "))
 
 if Nome == "Moqueca" or Nome == "moqueca":
     print("Categoria: Comida")
-elif Nome == "Ford" or Nome == "ford":
+if Nome == "Ford" or Nome == "ford":
     print("Categoria: Marca de carro")
 else:
     print("Categoria: Não Cadastrada")
